1841bc45b1735aeb6b42eea4bcbba6f591e6078e
jason-701 <101868817+jason-701@users.noreply.github.com>
2022-11-02 17:18:00 +0800

    yes

--- lab3/SC2001_lab_3/src/Main.java
+++ lab3/SC2001_lab_3/src/Main.java
@@ -4,15 +4,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int objNum;
+        int objNum,capacity;
         System.out.print("Enter the number of objects: ");
         objNum = scanner.nextInt();
         int[] objectArray = new int[objNum];
 
         int[] weightArray = new int[objNum];
+        int[] profitArray = new int[objNum];
         for (int i = 0; i < objNum; i++) {
             System.out.print("Enter the weight for object " + (i+1) + ": ");
             weightArray[i] = scanner.nextInt();
+            System.out.print("Enter the profit for object " + (i+1) + ": ");
+            profitArray[i] = scanner.nextInt();
         }
+        System.out.print("Enter the capacity: ");
+        capacity = scanner.nextInt();
+
+        int[][] output = bottomUp.knapSackBottomUp(weightArray,capacity,profitArray,objNum);
+        System.out.println(output[objNum][capacity]);
     }
 }
--- lab3/SC2001_lab_3/src/bottomUp.java
+++ lab3/SC2001_lab_3/src/bottomUp.java
@@ -1,2 +1,24 @@
+import java.lang.Math;
 public class bottomUp {
+    static int[][] knapSackBottomUp(int[] weight, int capacity,int[] profit, int objNum){
+        int[][] array = new int[objNum+1][capacity+1];
+        for (int i = 0; i < capacity+1; i++) {
+            array[0][i]=0;
+        }
+        for (int i = 0; i < objNum + 1; i++) {
+            array[i][0]=0;
+        }
+        for (int i = 1; i < objNum+1; i++) {
+            for (int j = 1; j < capacity+1; j++) {
+                array[i][j] = array[i-1][j];
+                if (j>=weight[i-1]){
+                    if (array[i][j]<array[i-1][j-weight[i-1]]+profit[i-1]){
+                        array[i][j]=array[i-1][j-weight[i-1]]+profit[i-1];
+                    }
+                }
+            }
+        }
+        return array;
+    }
+
 }
