b2bfe523baffa00012858ceb40dadb8bc8d41036
jason-701 <101868817+jason-701@users.noreply.github.com>
2022-11-02 16:28:24 +0800

    initial commit

--- lab3/SC2001_lab_3/src/Main.java
+++ lab3/SC2001_lab_3/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        int objNum;
+        System.out.print("Enter the number of objects: ");
+        objNum = scanner.nextInt();
+        int[] objectArray = new int[objNum];
+
+        int[] weightArray = new int[objNum];
+        for (int i = 0; i < objNum; i++) {
+            System.out.print("Enter the weight for object " + (i+1) + ": ");
+            weightArray[i] = scanner.nextInt();
+        }
     }
 }
